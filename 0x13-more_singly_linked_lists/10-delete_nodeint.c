be5e8578ad22d87bdd50e9da6920334dfb854f2b
Olarealcolly <olaoluwaoyeyipo@yahoo.com>
2022-04-25 22:38:58 -0700

    ola

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,53 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer
- * @idx : ist where the new node should be added
- * @n : data of the list
- * Return: address of the new node, or NULL if it failed
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	/*Declaring variables and lists*/
-	listint_t *new_node, *temp = *head;
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
 
-	/*Using malloc for list new_node*/
-	new_node = malloc(sizeof(listint_t));
+	h = *head;
 
-	/*Evaluate new_node*/
-	if (new_node == NULL)
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
+	}
+
+	if (h == NULL && idx != 0)
 		return (NULL);
 
-	/*Assign data*/
-	new_node->n = n;
-	new_node->next = NULL;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-	/*if idx is 0 so return value head*/
+	new->n = n;
+
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
 	}
-
-	/*WHILE It determines how far it prints the positions*/
-	while (i < (idx - 1))
+	else
 	{
-		temp = temp->next;
-
-		/*not possible to add the new node at index*/
-		if (temp == NULL && (idx - i) > 0)
-			return (NULL);
-
-		i++;
+		new->next = h->next;
+		h->next = new;
 	}
 
-	/*Swap*/
-	new_node->next = temp->next;
-	temp->next = new_node;
-
-	return (new_node);
+	return (new);
 }
